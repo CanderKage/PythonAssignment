aa89565db5a604d9ea1e07063060b4096a4940af
CanderKage <36044850+CanderKage@users.noreply.github.com>
2018-09-09 14:14:31 +0545

    Workshop

---
+++ workshop/modules/task.py
@@ -0,0 +1,34 @@
+"""
+Write a class in which its one method accepts a string from console and 
+another method to print the characters that have even indexes.
+
+Example:
+If the following string is given as input to the program:
+
+H1e2l3l4o5w6o7r8l9d
+
+Then, the output of the program should be:
+
+Helloworld
+
+"""
+
+"""
+Write a class in which its one method accepts a string from console and 
+another method to print the characters that have odd indexes.
+
+Example:
+If the following string is given as input to the program:
+
+Hello123
+
+Then, the output of the program should be:
+
+el13
+
+"""
+
+
+if __name__ == "__main__":
+    pass
+    
